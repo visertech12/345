18aac41cc5272b2e60ef8dbae64c7b9869fae550
visertech12 <i.am.visertech@gmail.com>
2025-07-15 14:28:25 +0500

    Add files via upload

---
+++ assets/pages-auth-register-register.DX2LtTlc.js
@@ -0,0 +1,1 @@
+import{t as e,w as t,u as r,v as s,y as a}from"./index-DJtkXLbk.js";import{_ as o}from"./_plugin-vue_export-helper.BCo6x5W8.js";const n=o({data:()=>({}),methods:{}},[["render",function(o,n,u,d,i,l){const p=r;return s(),e(p,null,{default:t((()=>[a(" register ")])),_:1})}]]);export{n as default};
